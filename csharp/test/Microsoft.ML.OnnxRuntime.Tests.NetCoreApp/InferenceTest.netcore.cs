8cac8f65d230c5fb364c66c847f3659aa34be849
Yi Zhang <zhanyi@microsoft.com>
2022-10-10 10:10:40 +0800

    skip opset < 7 or int8 and fp16 model

--- csharp/test/Microsoft.ML.OnnxRuntime.Tests.NetCoreApp/InferenceTest.netcore.cs
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests.NetCoreApp/InferenceTest.netcore.cs
@@ -439,9 +439,11 @@ namespace Microsoft.ML.OnnxRuntime.Tests
 
             foreach (var opsetDir in getOpsetDirectories(modelsDirInfo))
             {
+                var opset = opsetDir.Name;
+                var opset_num = Int32.Parse(opset.Remove(0,5));
                 foreach (var modelDir in opsetDir.EnumerateDirectories())
                 {
-                    if (skipModels.ContainsKey(modelDir.Name))
+                    if (skipModels.ContainsKey(modelDir.Name) || opset_num < 7 || modelDir.Name.Contains("int8") || modelDir.Name.Contains("fp16"))
                     {
                         //Console.WriteLine("Model {0} is skipped due to the error: {1}", modelDir.FullName, skipModels[modelDir.Name]);
                         yield return new object[] { modelDir.Parent.FullName, modelDir.Name };
