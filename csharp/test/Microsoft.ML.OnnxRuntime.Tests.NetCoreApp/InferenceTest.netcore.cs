702045d4c18bfc7f35317cde391e0dfc5f14e0bc
Yi Zhang <zhanyi@microsoft.com>
2022-10-13 20:05:23 +0800

    Revert "skip int8 and fp16 in C# test"
    This reverts commit 5fdc5dc95a812ba0f5e1b426d1df69ff8cb70273.

--- csharp/test/Microsoft.ML.OnnxRuntime.Tests.NetCoreApp/InferenceTest.netcore.cs
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests.NetCoreApp/InferenceTest.netcore.cs
@@ -420,12 +420,10 @@ namespace Microsoft.ML.OnnxRuntime.Tests
 
             foreach (var opsetDir in getOpsetDirectories(modelsDirInfo))
             {
-                var opset = opsetDir.Name;
-                var opset_num = Int32.Parse(opset.Remove(0,5));
                 //var modelRoot = new DirectoryInfo(Path.Combine(modelsDir, opsetDir.Name));
                 foreach (var modelDir in opsetDir.EnumerateDirectories())
                 {
-                    if (!(skipModels.ContainsKey(modelDir.Name) || modelDir.Name.Contains("int8") || modelDir.Name.Contains("fp16")))
+                    if (!skipModels.ContainsKey(modelDir.Name))
                     {
                         yield return new object[] { modelDir.Parent.FullName, modelDir.Name };
                     }
@@ -441,11 +439,9 @@ namespace Microsoft.ML.OnnxRuntime.Tests
 
             foreach (var opsetDir in getOpsetDirectories(modelsDirInfo))
             {
-                var opset = opsetDir.Name;
-                var opset_num = Int32.Parse(opset.Remove(0,5));
                 foreach (var modelDir in opsetDir.EnumerateDirectories())
                 {
-                    if (skipModels.ContainsKey(modelDir.Name) || modelDir.Name.Contains("int8") || modelDir.Name.Contains("fp16"))
+                    if (skipModels.ContainsKey(modelDir.Name))
                     {
                         //Console.WriteLine("Model {0} is skipped due to the error: {1}", modelDir.FullName, skipModels[modelDir.Name]);
                         yield return new object[] { modelDir.Parent.FullName, modelDir.Name };
