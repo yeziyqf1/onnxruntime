cf071746280d707eaeb5d413a672d18feae7b720
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2022-11-07 14:22:49 -0800

    11

--- cmake/onnxruntime_webassembly.cmake
+++ cmake/onnxruntime_webassembly.cmake
@@ -202,10 +202,9 @@ else()
                         -s VERBOSE=0                                                \
                         -s NO_FILESYSTEM=1                                          \
                         -s ASYNCIFY=1                                               \
-                        -s ASYNCIFY_STACK_SIZE=8192                                 \
+                        -s ASYNCIFY_STACK_SIZE=16384                                \
                         -s ASYNCIFY_ADVISE=1 -s ASYNCIFY_DEBUG=0 \
                         -s ASYNCIFY_IGNORE_INDIRECT=0 -s ASYNCIFY_ADD=OrtRun \
-                        --closure 1 \
                         --no-entry")
 
   if (onnxruntime_USE_JS)
